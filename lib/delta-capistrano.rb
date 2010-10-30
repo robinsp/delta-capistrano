730c5ccfdfe0410f67fd5d072f0b9934b5a300d7
Robin Spainhour <robin.spainhour@deltaprojects.se>
2010-10-30 08:25:18 +0200

    Mixin our module in Capistrano::Configuration

--- lib/delta-capistrano.rb
+++ lib/delta-capistrano.rb
@@ -1,2 +1,8 @@
-
+require 'capistrano'
 require 'delta_capistrano/configuration/actions/run_in_shell'
+
+module Capistrano
+  class Configuration
+    include DeltaCapistrano::Configuration::Actions::RunInShell
+  end
+end
--- spec/delta-capistrano_spec.rb
+++ spec/delta-capistrano_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "DeltaCapistrano" do
-  it "should include additional features on Capistrano::Configuration"
-
+  it "should include additional features on Capistrano::Configuration" do
+    Capistrano::Configuration.included_modules.should include DeltaCapistrano::Configuration::Actions::RunInShell
+  end
 end
